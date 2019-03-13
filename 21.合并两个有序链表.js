75c488e8c4efd78c3dbe9d92e38e3e79585d670c
zhanghao05 <cmcesummer@126.com>
2019-03-13 14:27:04 +0800

    update newest

--- 21.合并两个有序链表.js
+++ 21.合并两个有序链表.js
@@ -33,18 +33,15 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    let cache = [];
-    while (l2) {
-        const mid = new ListNode(l1.val);
-        mid.next = new ListNode(l2.val);
-        cache.push(mid);
-        l1 = l1.next;
-        l2 = l2.next;
+    if (!l1) return l2;
+    if (!l2) return l1;
+    let res;
+    if (l1.val <= l2.val) {
+        res = l1;
+        res.next = mergeTwoLists(l1.next, l2);
+    } else {
+        res = l2;
+        res.next = mergeTwoLists(l1, l2.next);
     }
-    if (!cache.length) return new ListNode();
-    const last = cache.reduceRight((pre, next) => {
-        next.next.next = pre;
-        return next;
-    });
-    return last;
+    return res;
 };
