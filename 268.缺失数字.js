51f62dcc1f43f098d368ee74ab0717963a3e1917
zhanghao05 <cmcesummer@126.com>
2019-04-24 20:30:19 +0800

    这个算法

--- 268.缺失数字.js
+++ 268.缺失数字.js
@@ -8,16 +8,24 @@
  * @return {number}
  */
 // O(2n)
+// var missingNumber = function(nums) {
+//     const max = nums.length;
+//     const map = {};
+//     for (let i = 0; i < max; i++) {
+//         map[nums[i]] = 1;
+//     }
+//     for (let i = 0; i <= max; i++) {
+//         if (!map[i]) {
+//             return i;
+//         }
+//     }
+//     return 0;
+// };
+// O（n）
 var missingNumber = function(nums) {
-    const max = nums.length;
-    const map = {};
-    for (let i = 0; i < max; i++) {
-        map[nums[i]] = 1;
+    let sum = 0;
+    for (let i = 1; i < nums.length + 1; i++) {
+        sum = sum + i - nums[i - 1];
     }
-    for (let i = 0; i <= max; i++) {
-        if (!map[i]) {
-            return i;
-        }
-    }
-    return 0;
+    return sum;
 };
